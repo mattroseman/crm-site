d3f5b341f61abc8f3418e8553bdbb8a5ed193dcd
Matthew Roseman <mroseman95@gmail.com>
2017-07-02 14:12:07 -0400

    got uuid's to work and adding and removing fields works correctly
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- src/components/contact_fields/StringArrayField.tsx
+++ src/components/contact_fields/StringArrayField.tsx
@@ -1,69 +1,90 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 
 export interface StringArrayFieldProps {
-    label: string
+    label: string;
     handleChange: (value: string[]) => void;
 }
 
 export interface StringArrayFieldState {
-    value: string[];
+    values: string[];
+    uuids: string[];
 }
 
 export default class StringArrayField extends React.Component<StringArrayFieldProps, StringArrayFieldState> {
     constructor(props: StringArrayFieldProps) {
         super(props);
         this.state = {
-            value: [''],
+            values: [''],
+            uuids: [_.uniqueId(this.props.label)],
         };
 
         this.handleArrayFieldElementChange = this.handleArrayFieldElementChange.bind(this);
+        this.handleAddButtonPress = this.handleAddButtonPress.bind(this);
+        this.handleDeleteButtonPress = this.handleDeleteButtonPress.bind(this);
     }
 
-    handleArrayFieldElementChange(index: number, value: string) {
-        let newState = this.state;
-        newState.value[index] = value;
-        this.setState(newState);
-    }
-
-    // TODO figure out adding and deleting array field elements
-    // Possibly have uneditable fields below the single editable one
-    render() {
-        const stringArrayFieldList = this.state.value.map((value: string, index: number) => {
-            return (
-                <div key={index.toString()} className="add-contact-array-field-element">
-                    <StringArrayFieldElement 
-                        arrayIndex={index} 
-                        handleChange={this.handleArrayFieldElementChange} 
-                    />
-                    <button 
-                        className="add-contact-array-field-button" 
-                        type="button"
-                        onClick={() => {
-                            let newValue = this.state.value;
-                            newValue.push('');
-                            this.setState({
-                                value: newValue,
-                            });
-                        }}
-                    >
-                        {index == this.state.value.length-1 ? '+' : 'x'}
-                    </button>
-                </div>
-            );
+    handleArrayFieldElementChange(uuid: string, newValue: string) {
+        this.setState({
+            values: this.state.values.map((value: string, index: number) => {
+                return this.state.uuids[index] == uuid ? newValue : value;
+            }),
         });
+    }
 
+    handleAddButtonPress(uuid: string) {
+        this.setState({
+            values: [''].concat(this.state.values),
+            uuids: [_.uniqueId(this.props.label)].concat(this.state.uuids),
+        });
+    }
+
+    handleDeleteButtonPress(uuid: string) {
+        // TODO remove this element with uuid
+        let index = this.state.uuids.findIndex((elementUUID: string) => {
+            return elementUUID == uuid;
+        });
+        let newValues = this.state.values;
+        newValues.splice(index, 1);
+        let newUUIDs = this.state.uuids;
+        newUUIDs.splice(index, 1);
+        this.setState({
+            values: newValues,
+            uuids: newUUIDs,
+        });
+    }
+
+    render() {
         return (
             <div className="add-contact-label-field">
                 <label className="add-contact-label">{this.props.label}</label>
-                <div className="add-contact-array-fields">{stringArrayFieldList}</div>
+                <div className="add-contact-array-fields">
+                    {
+                        this.state.values.map((value: string, index: number) => {
+                            return (
+                                <StringArrayFieldElement 
+                                    key={this.state.uuids[index]}
+                                    uuid={this.state.uuids[index]}
+                                    initValue={value} 
+                                    firstElement={index == 0} 
+                                    handleChange={this.handleArrayFieldElementChange}
+                                    handleButton={index ? this.handleDeleteButtonPress : this.handleAddButtonPress}
+                                />
+                            );
+                        })
+                    }
+                </div>
             </div>
         );
     }
 }
 
 interface StringArrayFieldElementProps {
-    arrayIndex: number;
-    handleChange: (index: number, value: string) => void;
+    uuid: string;
+    initValue: string;
+    firstElement: boolean;
+    handleChange: (uuid: string, newValue: string) => void;
+    handleButton: (uuid: string) => void;
 }
 
 interface StringArrayFieldElementState {
@@ -74,21 +95,30 @@ class StringArrayFieldElement extends React.Component<StringArrayFieldElementPro
     constructor(props: StringArrayFieldElementProps) {
         super(props);
         this.state = {
-            value: '',
+            value: props.initValue,
         };
     }
 
     render() {
         return (
-            <input
-                className="add-contact-field add-contact-array-field"
-                type="text"
-                onChange={(event) => {
-                    this.setState({
-                        value: event.target.value,
-                    }, () => this.props.handleChange(this.props.arrayIndex, this.state.value));
-                }}
-            />
+            <div key={this.props.uuid} className="add-contact-array-field-element">
+                <input
+                    className="add-contact-field add-contact-array-field"
+                    type="text"
+                    onChange={(event) => {
+                        this.setState({
+                            value: event.target.value,
+                        }, () => this.props.handleChange(this.props.uuid, this.state.value));
+                    }}
+                />
+                <button
+                    className="add-contact-array-field-button"
+                    type="button"
+                    onClick={() => this.props.handleButton(this.props.uuid)}
+                >
+                {this.props.firstElement ? '+' : 'x'}
+                </button>
+            </div>
         );
     }
 }
