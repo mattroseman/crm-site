fd6fc9eb4fa8a2df7fd6a054ea209461a83e99c0
Matthew Roseman <mroseman95@gmail.com>
2017-07-08 15:10:50 -0400

    renamed some of the css classNames for the header to match BEM standards
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,16 +4,16 @@ export default class Header extends React.Component<undefined, undefined> {
     render() {
         return (
             <div className="header">
-                <div className="header-left-side">
-                    <div className="header-title">
+                <div className="header__left-side">
+                    <div className="header__title">
                         Go Go CRM
                     </div>
                 </div>
-                <div className="header-right-side">
-                    <div className="header-sign-up">
+                <div className="header__right-side">
+                    <div className="header__sign-up">
                         Sign-Up
                     </div>
-                    <div className="header-log-in">
+                    <div className="header__log-in">
                         Log-In
                     </div>
                 </div>
